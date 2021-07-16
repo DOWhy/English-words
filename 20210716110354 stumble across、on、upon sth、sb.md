68d57d76650a61fa0fd736f6ec79da9fa53d71e6
john <john@git.com>
2021-07-16 11:51:58 +0800

    20210716115157

---
+++ 20210716110354 stumble across、on、upon sth、sb.md
@@ -0,0 +1,5 @@
+意外发现；偶然遇见
+to discover sth/sb unexpectedly
+
+[[20210716110436 Police have stumbled across a huge drugs ring.]]
+
--- python/obsidian_to_anki - 副本.py
+++ python/obsidian_to_anki - 副本.py
@@ -5,58 +5,13 @@ import os
 import re
 
 
-# 将 markdown 转换为 HTML，并且写入到 某个 文件中
-def markdown2htmlAndWrite2question(anki, path): # 参数是数组
-
-    content1 = "".join(anki) # 将列表变成一个字符串
-    content1 = content1.replace('@%```', '@%\n```')
-    content1 = content1.replace('<br>```', '<br>\n```')
-
-    # file7 = open("C:\\Users\\john\\Downloads\\bbbba.txt", 'w', encoding='utf-8')
-    # file7.write(content1)
-    # file7.close()
-
-
-    # extensions 参考： https://python-markdown.github.io/extensions/
-    html = markdown.markdown(content1, extensions=['fenced_code', 'codehilite', 'tables', 'nl2br', 'extra', 'abbr', 'def_list', 'footnotes', 'md_in_html', 'admonition', 'legacy_attrs', 'legacy_em', 'meta', 'sane_lists', 'smarty', 'toc',  'pymdownx.highlight', 'pymdownx.inlinehilite', 'pymdownx.tasklist', 'pymdownx.caret', 'sane_lists', 'mdx_truly_sane_lists']) # 'wikilinks', 这个是将 [[]] 转换为内部链接的。  'pymdownx.arithmatex', markdown 中的 latex 转换为 HTML
-
-    # file6 = open("C:\\Users\\john\\Downloads\\aaaab.txt", 'w', encoding='utf-8')
-    # file6.write(html)
-    # file6.close()
-
-    # 转换文件中的标签。虽然文件中标签的格式是：“#xxx”，但是python依然会将它识别为一级标题，转换成“<h1>”标签的形式，所以这里将<h1>标签进行转换
-    taglist = re.findall(r'<h1.*>.*</h1>', html)
-    if taglist != []:
-        j = 0
-        for j in range(len(taglist)):
-            if re.search(r'<h1.*>(.*)</h1>', html):
-                tag = re.search(r'<h1.*>(.*)</h1>', html).group(1)
-                html = re.sub(r'<h1.*>' + tag + '</h1>', '#' + tag, html)
-
-    html = html.replace('<br />\n', '@@%%')
-    html = html.replace('\n', '<br />')
-    html = html.replace('&lt;br&gt;', '')
-    html = html.replace('@@%%', '<br />')
-    html = html.replace('@%@%', '\n')
-    html = html.replace("@%", "\t")
-    html = html.replace('<p>', "")
-    html = html.replace('</p>', '')
-
-    file2 = open(path, 'w', encoding='utf-8')
-    file2.write(html)
-    file2.close()
-
-
 if __name__ == "__main__":
 
-    # anki = [] # 用来存放那些即将导入anki的 问答题
-    anki = ["aa@%", "bb@%", "cc@%@%"]
-
+    anki = []
 
     questionPath = 'C:\\Users\\john\\Downloads\\question.txt'
 
-
-    path = 'c:\\Users\\john\\Documents\\English words\\'
+    path = 'C:\\Users\\john\\my_works\\English words\\'
 
     fileList = os.listdir(path)
 
@@ -69,36 +24,21 @@ if __name__ == "__main__":
         if file.endswith('.md') and re.match(r'\d{14}', file):
 
             file1 = open(path + file, encoding='utf-8')
-            conlist = file1.readlines()
+            content = file1.read()
             file1.close()
 
+            content = content.replace('"', '""')
+            content = content.replace("'", "''")
+            content = content.replace('\n', '<br>')
 
-            aid = ''
-            question = ''
-            answer = ''
+            anki.append(file[0:14] + '\t' + file[15:-3] + '\t' + '"' + content + '"' + '\n')
 
-            i = 0
-            for i in range(len(conlist)):
-                if conlist[i] == '\n':
-                    answer = answer + '<br>'
-                elif conlist[i].startswith('Q：'): # 如果文件内容某行以 Q：开头，这个属于问题的一部分
-                    question = conlist[i][2:-1]
-                else:
-                    answer = answer + conlist[i]
+    file2 = open(questionPath, 'w', encoding='utf-8')
+    file2.writelines(anki)
+    file2.close()
 
 
-            aid = re.match(r'(\d{14})', file).group(1)
-            question = file[15:-3] + '<br>' + question
-
-
-            anki.append(aid + '@%') # @% 作为字段之间的分割
-            anki.append(question + "@%")
-            anki.append(answer + '@%@%') # @%@% 作为卡片之间的分割
-            answer = ""
             
-    
-    
-    markdown2htmlAndWrite2question(anki, questionPath)
 
 
 
